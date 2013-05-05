f0e68f9adcc582b04e609228bc7bbc99c4f4bb34
Sergey Chipiga <chipiga86@mail.ru>
2013-05-05 13:26:23 +0400

    add email in registration page

--- logolizer/auth/forms.py
+++ logolizer/auth/forms.py
@@ -1,10 +1,9 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 class SessionForm(forms.Form):
   username = forms.CharField()
   password = forms.CharField(widget=forms.PasswordInput())
 
-class RegistrationForm(forms.Form):
-  email = forms.EmailField()
-  username = forms.CharField()
-  password = forms.CharField(widget=forms.PasswordInput())
+class UserCreationForm(UserCreationForm):
+  email = forms.EmailField(required=True)
--- logolizer/auth/views.py
+++ logolizer/auth/views.py
@@ -5,7 +5,6 @@ from django.contrib import auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from logolizer.auth.forms import *
 from logolizer.log.forms import UploadForm
 from logolizer.log.models import Log
