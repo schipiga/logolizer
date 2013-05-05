7c5679dc9528237698354a5b6bb9a77af2faac72
Sergey Chipiga <chipiga86@mail.ru>
2013-05-05 13:54:44 +0400

    add debugger

--- logolizer/settings.py
+++ logolizer/settings.py
@@ -166,3 +166,22 @@ LOGGING = {
         },
     }
 }
+
+if DEBUG:
+  MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+  INSTALLED_APPS += ('debug_toolbar',)
+  INTERNAL_APPS = ('127.0.0.1', 'localhost',)
+  DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.cache.CacheDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+  )
+  DEBUG_TOOLBAR_CONFIG = {
+    'EXCLUDE_URLS': ('/admin',),
+    'INTERCEPT_REDIRECTS': False,
+  }
